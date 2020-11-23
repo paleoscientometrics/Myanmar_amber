e8a3bde316420605fe27cb0074ed770c40a977c1
emmadunne <dunne.emma.m@gmail.com>
2020-11-23 14:08:10 +0000

    Added script to deal with PBDB aff data

---
+++ code/PBDB_data.R
@@ -0,0 +1,14 @@
+
+library(tidyverse)
+library(chronosphere)
+
+
+fetch("pbdb")
+
+## Import subset of "affilation data" that has Myanmar as the sampling country:
+aff_data <- read.csv("./data/aff_data_Myanmar.csv")
+glimpse(aff_data)
+
+## add column "amber" to indicate if the ref title contains a mention of 'amber'
+aff_data$amber <- ifelse(grepl("amber", aff_data$reftitle, ignore.case = T), "yes", "no")
+glimpse(aff_data)
