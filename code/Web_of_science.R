b6328da5a09b1d15bdef320d19ef5ccbdf4321dc
emmadunne <dunne.emma.m@gmail.com>
2020-11-30 19:00:18 +0000

    Plotting

--- code/Web_of_science.R
+++ code/Web_of_science.R
@@ -3,7 +3,8 @@
 #  Web of Science data
 #
 # _________________________
-
+install.packages("esquisse")
+esquisse::esquisser()
 library(tidyverse)
 
 ## Import full dataset
@@ -28,7 +29,22 @@ pubs_per_yr
 
 
 
+## Lollipop, lollipop, oh lolly lolly lollipop :)
 
+pubs_per_yr_lollipop <- ggplot() +
+  geom_col(data=WOS_data_30, aes(x=Publication.Year, y = 1:nrow(WOS_data_30))) +
+  geom_point(data = WOS_data_30, aes(Publication.Year, 1:nrow(WOS_data_30)), colour = "orange", size = 4) +
+  #geom_histogram(col="#9B4F0C", fill="#CE6B12", bins = 30) + # Canva social science lesson colour scheme
+  theme_minimal() + labs(x = "Publication year", y = "Number of publications") #+
+  #scale_x_continuous(breaks = seq(1990, 2020, 5), expand = c(0, 0)) +
+  #scale_y_continuous(breaks = seq(0, 144, 20), expand = c(0, 0))
+pubs_per_yr_lollipop
+
+
+flights_delay %>%
+  ggplot(aes(x=carrier,y=avg_arr_delay)) +
+  geom_col()+
+  labs(y= "Mean Arrival Delay", x="Carrier")
 
 
 
