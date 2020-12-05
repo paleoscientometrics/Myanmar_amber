41d3900cf50965f84a765ac032bf238331052529
Nussaibah Raja <nussaibah.raja.schoob@fau.de>
2020-12-05 11:15:59 +0100

    initial map with semi circles

---
+++ code/map_semi_circles.R
@@ -0,0 +1,83 @@
+## Load packages
+library(tidyverse)
+library(ggforce)
+library(extrafont)
+
+## Import subset of "affilation data" that has Myanmar as the sampling country:
+aff_data <- read.csv(file.path("data", "aff_data_Myanmar.csv"))
+aff_data <- data.frame(apply(aff_data, 2, function(x) gsub("United States", "USA", x)))
+
+colnames(aff_data)[1] <- "reference_no"
+
+# reshape affiliation data
+affs <- reshape2::melt(aff_data %>% dplyr::select(-pubyr, -author1last, -author2last, 
+                                          -reftitle, -doi, -publication_type, -complete_aff, -grid_code, -journal, -ethics), 
+               id.vars=c("reference_no", "samp_country"), 
+               value.name="aff_country") %>% 
+  dplyr::select(-variable) %>%
+  filter(!aff_country %in% c("Unknown", ""))
+
+
+## add column "amber" to indicate if the ref title contains a mention of 'amber'
+aff_data$amber <- ifelse(grepl("amber", aff_data$reftitle, ignore.case = T), "yes", "no")
+
+affs <- na.omit(merge(affs,aff_data[,c("reference_no", "amber")]))
+affs <- affs %>% group_by(aff_country, amber) %>% 
+  tally() %>% 
+  pivot_wider(names_from = amber,
+              values_from = n) %>% 
+  mutate(yes = replace_na(yes, 0),
+         no = replace_na(no, 0)
+  )
+
+affs <- affs %>% pivot_longer(c(yes, no), 
+                      names_to="amber",
+                      values_to="count")
+
+# Get centroid of locations
+world_map <- map_data("world")
+n <- which(world_map$subregion == "Hong Kong")
+world_map$region[n] <- "Hong Kong"
+
+# Retrievethe map data
+countries <- subset(world_map, region %in% unique(affs$aff_country))
+
+# Compute the centroid as the mean longitude and lattitude
+# Used as label coordinate for country's names
+region.lab.data <- countries %>%
+  group_by(region) %>%
+  summarise(long = mean(long), lat = mean(lat))
+
+affs <- merge(affs, region.lab.data, by.x="aff_country", by.y="region")
+
+aff.plot <- affs %>%  arrange(aff_country, desc(amber))
+
+aff.plot$start <- rep(c(0, pi), nrow(affs)/2)
+
+r <- 15 #radius of biggest circle
+scale <- r/max(sqrt(aff.plot$count))
+
+theme_set(theme_void(base_size=14) %+replace%
+            theme(legend.title = element_text(
+              face="bold", size=12),
+              text=element_text(family="Roboto"),
+              axis.title = element_text(face="bold"),
+              legend.position="bottom",
+              plot.background = element_rect(fill="#f5f5ef", colour=NA),
+              plot.title = element_text(face="bold", hjust=0.5, size=14))
+)
+
+x11();ggplot(w=10, h=5) +
+geom_polygon(data=world_map, aes(x = long, y = lat, group = group), 
+             fill="#ded4c5ff", colour = "#f5f5ef", size=0.5) +
+  geom_polygon(data=world_map[world_map$region == "Myanmar",], aes(x = long, y = lat, group = group), 
+               fill="#dd9c69ff", colour = "#d07120ff", size=0.5) +
+  geom_arc_bar(data=aff.plot, aes(x0 = long, y0 = lat, r0 = 0, r = sqrt(count)*scale,
+                   start = start, end = start + pi, fill = amber), alpha=0.9,
+               color = "#f5f5ef", size=0.5) +
+  labs(x="", y="", fill="Publications based \non amber inclusions") +
+  scale_fill_manual(values=c("#2d1b0cff", "#d07120ff"), labels=c("No", "Yes")) +
+  geom_text(data = aff.plot[aff.plot$count > 20 & duplicated(aff.plot$aff_country)== FALSE,],
+            aes(label = aff_country, x = long, y = lat + scale*sqrt(count) + .05),
+            size =11/.pt, vjust = 0, hjust=-0.2, family="Roboto", fontface=2) +
+  coord_equal(ratio = 1.2)
