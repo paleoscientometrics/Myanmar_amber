f5d1824f80184cf2f79171c0959515c221493dd3
Nussaibah Raja <nussaibah.raja.schoob@fau.de>
2020-12-06 21:24:28 +0100

    Timeline added, Map of researchers & some funders categorised

--- code/Web_of_science.R
+++ code/Web_of_science.R
@@ -6,7 +6,6 @@
 
 library(tidyverse)
 
-
 ## Import full dataset
 WOS_data <-  read.csv("./data/webofscience.csv", skip = 4, header = T)
 glimpse(WOS_data) # have a peek
@@ -14,7 +13,7 @@ hist(WOS_data$Publication.Year, breaks = 200) # crude histogram of all data
 
 
 ## Subset to last 30 years:
-WOS_data_30 <- subset(WOS_data, Publication.Year >+ 1990)
+WOS_data_30 <- subset(WOS_data, Publication.Year >+ 1989)
 hist(WOS_data_30$Publication.Year, breaks = 30)
 
 
@@ -26,10 +25,7 @@ pubs_per_yr <- ggplot(data=WOS_data_30, aes(Publication.Year)) +
   scale_x_continuous(breaks = seq(1990, 2020, 5), expand = c(0, 0)) +
   scale_y_continuous(breaks = seq(0, 144, 20), expand = c(0, 0))
 pubs_per_yr
-
 ggsave("./plots/pubs_per_yr_hist.pdf", plot = pubs_per_yr, width = 30, height = 12, units = "cm")
-
-
 
 ## Lollipop, lollipop, oh lolly lolly lollipop :)
 
@@ -51,5 +47,83 @@ pubs_per_yr_lollipop
 ggsave("./plots/pubs_per_yr_lollipop.pdf", plot = pubs_per_yr_lollipop, width = 25, height = 15, units = "cm")
 
 
+#PaleoPercs
+WOS_summary <- WOS_data_30 %>% group_by(Publication.Year) %>% 
+  tally() %>% 
+  mutate(ra = caTools::runmean(n, 3, alg="C"))
 
+theme_set(theme_minimal(base_size=14) %+replace%
+            theme(legend.title = element_text(
+              face="bold"),
+              text=element_text(family="Roboto"),
+              axis.title = element_text(face="bold"),
+              legend.position="bottom",
+              plot.background = element_rect(fill="#f5f5ef", colour=NA),
+              plot.title = element_text(face="bold", hjust=0, size=16))
+)
+
+timeline <- read.csv(file.path("data", "timeline.csv"))
+timeline <- timeline[timeline$Events != "",]
+
+timeline <- merge(WOS_summary, timeline[,c(1,2)], by.x="Publication.Year", by.y="Year")
+
+google <- read.csv(file.path("data", "google_trends.csv"), skip=2)
+colnames(google) <- c("Date", "Freq")
+google$year <- gsub(".*/(\\d+{4})$", "\\1", google$Date)
+
+google_summary <- google %>% group_by(year) %>% 
+  summarise(n=sum(Freq))
+
+ggplot(data=WOS_summary, aes(x=Publication.Year, y=ra)) + 
+  geom_line(size=1, col="#d07120ff") + 
+  geom_point(size=3, col="#d07120ff") +
+  geom_text(data=timeline, aes(x=Publication.Year, y=ra, label=Events), inherit.aes = FALSE)+
+  labs(x="Year of publication", 
+       y="Number of Publications",
+       subtitle = "\nMoving average of 3 years to take into consideration the publication process.\n") +
+  ggtitle("Number of publications on amber fossils") 
+
+ggsave(file.path("plots", "PaleoPercs_timeline.svg"), 
+       w=8, h=5
+)
+
+# Funding
+
+funding_organisations <- WOS_data_30[grep("amber", WOS_data_30$Article.Title),]$Funding.Orgs
+funding_organisations <- funding_organisations[funding_organisations!= ""]
+funding_organisations <- unlist(strsplit(funding_organisations, ";"))
+
+funding_organisations <- gsub("(\\[.*\\])", "", funding_organisations) # remove funding numbers
+
+
+funders <- data.frame(table(funding_organisations), stringsAsFactors = FALSE)
+funders$funding_organisations <- as.character(funders$funding_organisations)
+
+funders$country <- NA
+funders$country[grep("China|Zhejiang|Chang-jiang|Shandong|CAS|Gansu|Tibet|Chinese|Beijing|Jiangsu|Shanghai|Capital Normal University|Guizhou|Nanjing|Guangzhou|Qingdao|Heibei", funders$funding_organisations)] <- "China"
+funders$country[grep("Russia", funders$funding_organisations)] <- "Russia"
+funders$country[grep("Japan", funders$funding_organisations)] <- "Japan"
+funders$country[grep("DFG|Berlin|Alexander von Humboldt|German|Volkswagen", funders$funding_organisations)] <- "Germany"
+funders$country[grep("Czech|Prague", funders$funding_organisations)] <- "Czechia"
+funders$country[grep("Canada|Alberta", funders$funding_organisations)] <- "Canada"
+funders$country[grep("Spain|Spanish", funders$funding_organisations)] <- "Spain"
+funders$country[grep("NSF|U\\.S\\.|Utah|Paleontological Society|New York|Tennessee|US", funders$funding_organisations)] <- "USA"
+funders$country[grep("Poland|Polish|Katowice", funders$funding_organisations)] <- "Poland"
+funders$country[grep("CNRS|FNRS|Rennes|France|French", funders$funding_organisations)] <- "France"
+funders$country[grep("Australia|Adelaide|Sydney", funders$funding_organisations)] <- "Australia"
+funders$country[grep("CNPq|FAPESP", funders$funding_organisations)] <- "Brazil"
+funders$country[grep("Slovak", funders$funding_organisations)] <- "Slovakia"
+funders$country[grep("Europe", funders$funding_organisations)] <- "EU"
+funders$country[grep("King Saud", funders$funding_organisations)] <- "Saudi Arabia"
+funders$country[grep("KwaZulu", funders$funding_organisations)] <- "South Africa"
+funders$country[grep("UNESCO", funders$funding_organisations)] <- "UN"
+funders$country[grep("Hungar", funders$funding_organisations)] <- "Hungary"
+funders$country[grep("HKU", funders$funding_organisations)] <- "Hong Kong"
+funders$country[grep("Leban", funders$funding_organisations)] <- "Lebanon"
+
+
+funders$funding_organisations[is.na(funders$country)]
+
+#manual
+write.csv(funders, file.path("output", "funding_organisations.csv"), row.names = FALSE)
 
--- code/map_semi_circles.R
+++ code/map_semi_circles.R
@@ -2,6 +2,8 @@
 library(tidyverse)
 library(ggforce)
 library(extrafont)
+library(grid)
+library(gridExtra)
 
 ## Import subset of "affilation data" that has Myanmar as the sampling country:
 aff_data <- read.csv(file.path("data", "aff_data_Myanmar.csv"))
@@ -11,9 +13,9 @@ colnames(aff_data)[1] <- "reference_no"
 
 # reshape affiliation data
 affs <- reshape2::melt(aff_data %>% dplyr::select(-pubyr, -author1last, -author2last, 
-                                          -reftitle, -doi, -publication_type, -complete_aff, -grid_code, -journal, -ethics), 
-               id.vars=c("reference_no", "samp_country"), 
-               value.name="aff_country") %>% 
+                                                  -reftitle, -doi, -publication_type, -complete_aff, -grid_code), 
+                       id.vars=c("reference_no", "samp_country"), 
+                       value.name="aff_country") %>% 
   dplyr::select(-variable) %>%
   filter(!aff_country %in% c("Unknown", ""))
 
@@ -31,8 +33,8 @@ affs <- affs %>% group_by(aff_country, amber) %>%
   )
 
 affs <- affs %>% pivot_longer(c(yes, no), 
-                      names_to="amber",
-                      values_to="count")
+                              names_to="amber",
+                              values_to="count")
 
 # Get centroid of locations
 world_map <- map_data("world")
@@ -52,32 +54,92 @@ affs <- merge(affs, region.lab.data, by.x="aff_country", by.y="region")
 
 aff.plot <- affs %>%  arrange(aff_country, desc(amber))
 
-aff.plot$start <- rep(c(0, pi), nrow(affs)/2)
+aff.plot$start <- rep(c(-pi/2, pi/2), nrow(affs)/2)
 
 r <- 15 #radius of biggest circle
 scale <- r/max(sqrt(aff.plot$count))
 
-theme_set(theme_void(base_size=14) %+replace%
+theme_set(theme_void(base_size=10) %+replace%
             theme(legend.title = element_text(
-              face="bold", size=12),
+              face="bold", size=8),
+              legend.text = element_text(
+                size=8),
               text=element_text(family="Roboto"),
               axis.title = element_text(face="bold"),
               legend.position="bottom",
               plot.background = element_rect(fill="#f5f5ef", colour=NA),
-              plot.title = element_text(face="bold", hjust=0.5, size=14))
+              plot.title = element_text(face="bold", hjust=0, size=14))
 )
 
-x11();ggplot(w=10, h=5) +
-geom_polygon(data=world_map, aes(x = long, y = lat, group = group), 
-             fill="#ded4c5ff", colour = "#f5f5ef", size=0.5) +
+countries.lab <- aff.plot[aff.plot$count > 20 & aff.plot$amber == "yes",]$aff_country
+countries.lab <- c(countries.lab, "Myanmar")
+
+p <- ggplot() +
+  geom_polygon(data=world_map, aes(x = long, y = lat, group = group), 
+               fill="#ded4c5ff", colour = "#f5f5ef", size=0.3) +
   geom_polygon(data=world_map[world_map$region == "Myanmar",], aes(x = long, y = lat, group = group), 
-               fill="#dd9c69ff", colour = "#d07120ff", size=0.5) +
+               fill="#dd9c69ff", colour = "#d07120ff", size=0.3) +
   geom_arc_bar(data=aff.plot, aes(x0 = long, y0 = lat, r0 = 0, r = sqrt(count)*scale,
+                                  start = start, end = start + pi, fill = amber), alpha=0.9,
+               color = "#f5f5ef", size=0.3) +
+  labs(x="", y="", fill="Publications based \non amber inclusions",
+       subtitle="A comparison between fieldwork done on amber vs non-amber fossils") +
+  scale_fill_manual(values=c("#2d1b0cff", "#d07120ff"), labels=c("No", "Yes")) +
+  geom_text(data = aff.plot[aff.plot$aff_country %in% countries.lab &  duplicated(aff.plot$aff_country)== FALSE,],
+            aes(label = aff_country, x = long, y = lat + scale*sqrt(count) + .05),
+            size =2.5, vjust = 0, hjust=-0.2, family="Roboto", fontface=2) +
+  
+  geom_text(data = aff.plot[aff.plot$amber == "no" & aff.plot$aff_country %in% countries.lab,],
+            aes(label = count, x = long, y = lat + scale*sqrt(count) + .05), size=2, col="#d07120ff", 
+            vjust = 4, hjust=0.5, family="Roboto", fontface=2) +
+  geom_text(data = aff.plot[aff.plot$count > 20 & aff.plot$amber == "yes",],
+            aes(label = count, x = long, y = lat + scale*sqrt(count) + .05), size=2, col="#2d1b0cff", 
+            vjust = 2, hjust=0.5, family="Roboto", fontface=2) +
+  coord_equal(ratio = 1.2) +
+  ggtitle("Locations of researchers carrying out fieldwork in Myanmar")
+
+
+
+# Europe
+
+r2 <- 5 #radius of biggest circle
+scale2 <- r2/max(sqrt(aff.plot$count))
+
+p_eu <- ggplot() +
+  geom_polygon(data=world_map, aes(x = long, y = lat, group = group), 
+               fill="#ded4c5ff", colour = "#f5f5ef", size=0.5) +
+  geom_arc_bar(data=aff.plot, 
+               aes(x0 = long, y0 = lat, r0 = 0, r = sqrt(count)*scale2,
                    start = start, end = start + pi, fill = amber), alpha=0.9,
                color = "#f5f5ef", size=0.5) +
-  labs(x="", y="", fill="Publications based \non amber inclusions") +
-  scale_fill_manual(values=c("#2d1b0cff", "#d07120ff"), labels=c("No", "Yes")) +
-  geom_text(data = aff.plot[aff.plot$count > 20 & duplicated(aff.plot$aff_country)== FALSE,],
+  labs(x="", y="", fill="") +
+  scale_fill_manual(values=c("#2d1b0cff", "#d07120ff"), guide=FALSE)+
+  geom_text(data = aff.plot[duplicated(aff.plot$aff_country)==FALSE & aff.plot$count > 5,],
             aes(label = aff_country, x = long, y = lat + scale*sqrt(count) + .05),
-            size =11/.pt, vjust = 0, hjust=-0.2, family="Roboto", fontface=2) +
-  coord_equal(ratio = 1.2)
+            size =2.5, vjust = 4, hjust=-0.2, family="Roboto", fontface=2) +
+  
+  # number labels for no amber
+  geom_text(data = aff.plot[aff.plot$amber == "no"& aff.plot$count > 5,],
+            aes(label = count, x = long, y = lat + scale*sqrt(count) + .05), size=2, col="#d07120ff", 
+            vjust = -5, hjust=0.5, family="Roboto", fontface=2) +
+  #number labels for amber
+  geom_text(data = aff.plot[aff.plot$amber == "yes"& aff.plot$count > 5,],
+            aes(label = count, x = long, y = lat + scale*sqrt(count) + .05), 
+            size=2, col="#2d1b0cff", 
+            vjust = 0, hjust=1, family="Roboto", fontface=2) +
+  coord_equal(ratio = 1.2, xlim=c(-9.5, 28.2), ylim=c(35, 61)) 
+
+layout=rbind(c(1, 1, 1,1,1,1),
+             c(2, 2, 1,1,1,1),
+             c(2, 2, 1,1,1,1))
+
+
+map <- grid.arrange(p + annotate("rect", xmin=-9.5, xmax=28.2, ymin=35, ymax=61, fill=NA, col="#2d1b0cff"), p_eu, 
+                    layout_matrix = layout)
+
+
+ggsave(file.path("plots", "map_semi_circles.svg"), p, w=8, height=5)
+
+svg(file.path("plots", "map_semi_circles_eu.svg"),width = 8, height=5)
+plot(map) 
+dev.off()
